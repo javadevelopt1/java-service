1fa4df0abf149292fc84c7a8bf61bfe609dbe9df
Carlos <carlos.cruz@atud.com.br>
2019-03-19 21:02:49 -0300

    versao 1

---
+++ src/main/java/com/develop/example/javaservice/JavaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.develop.example.javaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaServiceApplication.class, args);
+	}
+
+}
